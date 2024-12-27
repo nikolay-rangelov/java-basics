20c0d501a610357bdd32f082a89b3cc13bde56d0
nikolay-rangelov <nikolai.rangeloff@gmail.com>
2024-12-27 14:57:54 +0200

    T05-06_IfElseStatements - Exercises: 8: DONE

---
+++ T05-06_IfElseStatements/src/Exercises/Ex00_Testing.java
@@ -0,0 +1,19 @@
+package Exercises;
+
+public class Ex00_Testing {
+    public static void main(String[] args) {
+        /*
+        for (int i = 1; i <= 3; i++) { // Outer loop
+            for (int j = 1; j <= 3; j++) { // Inner loop
+                if (j == 2) {
+                    break; // Breaks only the inner loop
+                }
+                System.out.println("i = " + i + ", j = " + j);
+            }
+            System.out.println("Outer loop continues with i = " + i);
+        }
+
+         */
+        System.out.println(Math.ceil(9.1));
+    }
+}
--- T05-06_IfElseStatements/src/Exercises/Ex08_LunchBreak.java
+++ T05-06_IfElseStatements/src/Exercises/Ex08_LunchBreak.java
@@ -5,6 +5,18 @@ import java.util.Scanner;
 public class Ex08_LunchBreak {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String movie = sc.nextLine();
+        double movieDuration = Integer.parseInt(sc.nextLine());
+        double breakDuration = Double.parseDouble(sc.nextLine());
 
+        double lunchDuration = breakDuration * 1.0/8.0;
+        double recreationTime = breakDuration * 0.25;
+        double timeLeft = breakDuration - lunchDuration - recreationTime;
+
+        if(timeLeft >= movieDuration) {
+            System.out.printf("You have enough time to watch %s and left with %d minutes free time.", movie, (int)Math.ceil(timeLeft - movieDuration));
+        } else {
+            System.out.printf("You don't have enough time to watch %s, you need %d more minutes.", movie, (int)Math.ceil(movieDuration - timeLeft));
+        }
     }
 }
